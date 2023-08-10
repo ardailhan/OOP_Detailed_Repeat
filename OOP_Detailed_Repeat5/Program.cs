89f0a53ab613e9a5d0413f6b94acc8d141710774
ardailhan <ardailhan_@outlook.com>
2023-08-10 13:47:28 +0300

    object concept

---
+++ OOP_Detailed_Repeat5/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        new MyClass();
+
+        new Random();
+
+        new MyClass();
+        new MyClass();
+        new MyClass();
+        new MyClass();
+
+
+        MyClass myClass = new(); //Target-typed New Expression
+    }
+}
+
+class MyClass
+{
+    public int A { get; set; }
+    public void X()
+    {
+
+    }
+}
