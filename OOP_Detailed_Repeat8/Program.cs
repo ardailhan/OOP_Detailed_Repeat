ebd8ee76c1ad2de67ab8ebf6d2624d2168188e69
ardailhan <ardailhan_@outlook.com>
2023-08-10 19:34:54 +0300

    Encapsulation

---
+++ OOP_Detailed_Repeat8/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass m = new MyClass();
+        m.ASet(15);
+        Console.WriteLine(m.AGet());
+
+        m.A = 123321;
+        Console.WriteLine(m.A);
+    }
+}
+
+class MyClass
+{
+    int a;
+    #region Eski Tip Encapsulation
+    public int AGet()
+    {
+
+        return a;
+    }
+    public void ASet(int value)
+    {
+        a = value;
+    }
+    #endregion
+    #region Full Property ile Encapsulation
+    public int A
+    {
+        get { return a; }
+        set { a = value; }
+    }
+
+    #endregion
+}
