0604a3d9319e74d14b52ddab2e703d2cac88d90e
ardailhan <ardailhan_@outlook.com>
2023-08-10 11:47:38 +0300

    class members done

---
+++ OOP_Detailed_Repeat3/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        #region Class İçerisinde Tanımlanan Class Sınıf Elemanı Mıdır ?
+        //MyClass m1 = new MyClass();
+
+        //MyClass.MyClass2 m2 = new MyClass.MyClass2();
+        #endregion
+        #region Class Elemanlarına Açıklama Satırı Nasıl Eklenir ?
+        Random random = new Random();
+        
+        MyClass myClass = new MyClass();
+        myClass.X();
+        #endregion
+    }
+}
+
+/// <summary>
+/// Example Class
+/// </summary>
+class MyClass
+{
+    int a;
+    /// <summary>
+    /// This is a property
+    /// </summary>
+    public int MyProperty { get; set; }
+    /// <summary>
+    /// This is an example method
+    /// </summary>
+    public void X() { }
+    /// <summary>
+    /// This is an example method with an overload
+    /// </summary>
+    /// <param name="a parameter"></param>
+    public void X(int a) { }
+    public int this[int a]
+    {
+        get { return 0; }
+    }
+
+    public class MyClass2
+    {
+
+    }
+}
---
+++ OOP_Detailed_Repeat4/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        #region this Keyword
+
+        #region 1. Sınıfın Nesnesini Temsil Eder
+        //MyClass m1 = new MyClass();
+        //MyClass m2 = new MyClass();
+
+
+        #endregion
+        #region 2. Aynı İsimde Field ve Metot Parametrelerini Ayırmak İçin Kullanılır
+        //this keywordü ilgili class yapılanmasının o anki nesnesine karşılık gelir.
+        //this kullanmak zorunda değiliz.
+        #endregion
+        #region 3. Bir Constructor'dan Başka Bir Constructor'ı Çağırmak İçin Kullanılır
+
+        #endregion
+
+        #endregion
+
+    }
+}
+
+class MyClass
+{
+    int a;
+    public void X()
+    {
+
+    }
+}
