d7a129b7f906bf919acee5a9ffb100cd9553daa3
ardailhan <ardailhan_@outlook.com>
2023-08-10 18:54:20 +0300

    Shallow and Deep copy

---
+++ OOP_Detailed_Repeat7/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        #region Shallow 
+        #region Example1
+        //MyClass m1 = new MyClass();
+        //MyClass m2 = m1;
+        //MyClass m3 = m2;
+        //MyClass m4 = new MyClass();
+        #endregion
+        #region Example2
+        //MyClass m1 = null;
+        //MyClass m2 = new MyClass();
+        //MyClass m3 = m2;
+        //m1 = m3;
+        #endregion
+        #region Example3
+        //MyClass m1 = new MyClass();
+        //MyClass m2 = new MyClass();
+        //MyClass m3 = m1;
+        //m1 = m2;
+        //m2 = m1;
+        //m1 = m1;
+        #endregion
+        #endregion
+        #region Deep Copy
+        MyClass m1 = new MyClass();
+        MyClass m2 = m1; //Shallow Copy
+        MyClass m3 = m1.Clone(); //Deep Copy
+        #endregion
+    }
+}
+
+class MyClass
+{
+    public MyClass Clone()
+    {
+        return (MyClass)this.MemberwiseClone();
+    }
+}
