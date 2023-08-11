597000d154a100556855c477167a45808a8102ff
ardailhan <ardailhan_@outlook.com>
2023-08-11 12:21:03 +0300

    Record and with expression lesson

---
+++ OOP_Detailed_Repeat9/Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //MyClass m1 = new MyClass()
+        //{
+        //    XXX = 5
+        //};
+        //MyClass m2 = new MyClass()
+        //{
+        //    XXX = 5
+        //};
+
+        //Console.WriteLine(m1.Equals(m2));
+
+        //MyRecord m3 = new MyRecord()
+        //{
+        //    XXX = 5
+        //};
+        //MyRecord m4 = new MyRecord() 
+        //{
+        //    XXX = 5 
+        //};
+        //Console.WriteLine(m3.Equals(m4));
+
+        //MyClass m = new MyClass()
+        //{
+        //    XXX = 5,
+        //    XXX2 = 10
+        //};
+        //m.XXX2 = 15;
+        //MyClass m2 = m.With(15);
+
+        MyRecord m = new MyRecord()
+        {
+            XXX = 5,
+            XXX2 = 10
+        };
+        MyRecord m2 = m with { XXX2 = 15 };   //With Expression
+    }
+}
+
+record MyRecord
+{
+    public int XXX { get; init; }
+    public int XXX2 { get; init; }
+}
+class MyClass
+{
+    public int XXX { get; init; }
+    public int XXX2 { get; init; }
+    public MyClass With(int xxx2)
+    {
+        return new MyClass
+        {
+            XXX = this.XXX,
+            XXX2 = xxx2
+        };
+    }
+}
