1a7d726fb798bea86ec1e10f3b161d4a90092754
ardailhan <ardailhan_@outlook.com>
2023-08-16 10:11:55 +0300

    Deconstructor Method

---
+++ OOP_Detailed_Repeat12/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            Name = "Arda",
+            Age = 15
+        };
+
+        var (x, y) = person;
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public void Deconstruct(out string name, out int age)
+    {
+        name = Name;
+        age = Age;
+    }
+}
