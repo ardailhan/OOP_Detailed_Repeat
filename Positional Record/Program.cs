5e8a606e27cb9a930c74098ec864a844536af5a7
ardailhan <ardailhan_@outlook.com>
2023-08-16 14:33:00 +0300

    Positional Record

---
+++ Positional Record/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        MyRecord m = new MyRecord();
+        var(n,s) = m;
+    }
+}
+
+record MyRecord(string Name, string Surname) //Positional Record
+{
+    public MyRecord() : this("asdasd", "dsadsa")
+    {
+        
+    }
+    public MyRecord(string Name) : this()
+    {
+        
+    }
+    //public string Name => name;
+    //public string Surname => surname;
+}
+
+
+
+//Eğer record parametre almışsa bu semantik positional record özelliğidir.
+//Bir record üzerinden ctor ve dector yapılanmasını hızlı bir şekilde olusturmamizi saglayan bir semantik saglamaktadır.
+//Positional Record kullanırken parametrelerin karsılıkları olan propertyleri manuel olusturmak zorunda değiliz.
+//Parametrelerin karsılıgı olarak compiler seviyesinde propertyler otomatik olusturulacaktır.
+//Bu propertyler olusuturulurken init olacak sekılde olusturulur.
+//Positional record tanımlanmıssa eger nesne uretımınde tetkıklenmesı/kullanılması zorunludur!
