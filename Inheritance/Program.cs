132b58d64af3bf97aa392c785ada8656de5d2c34
ardailhan <ardailhan_@outlook.com>
2023-08-16 15:57:22 +0300

    Inheritance Part I

---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Accountant accountant = new Accountant();
+        accountant.Name = "Test";
+        accountant.Advisor = true;
+        Developer developer = new Developer();
+        //developer.UsedLanguage
+    }
+}
+
+class Staff
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public bool MaritalStatus { get; set; }
+}
+
+class Accountant : Staff
+{
+    public bool Advisor { get; set; }
+}
+
+class Developer : Staff
+{
+    public string[] UsedLanguage { get; set; }
+}
+
+class Technician : Staff
+{
+    
+}
