4934b2a59297bc02a2e1d4696c1780d6f9a9d4ad
ardailhan <ardailhan_@outlook.com>
2023-08-17 12:52:18 +0300

    Inheritance done

---
+++ Inheritance3/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        D d = new();
+        //var x = d.X;  //D classından gelen X fonksiyonu
+    }
+}
+class A
+{
+    public void X()
+    {
+
+    }
+}
+class B : A
+{
+
+}
+class C : B
+{
+
+}
+class D : C
+{
+    public new void X() //Eskiden name hiding için new keywordü ile kullanmak zorundaydık aksi halde hata alırdık.
+    {
+
+    }
+}
