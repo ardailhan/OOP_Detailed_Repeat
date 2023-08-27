6dfab910dfe30402be86f8128e105ba5f4655d21
ardailhan <ardailhan_@outlook.com>
2023-08-27 19:13:17 +0300

    virtual and override lesson

---
+++ VirtualAndOverride/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Terlik t = new();
+        t.Bilgi();
+
+        Kalem k = new();
+        k.Bilgi();
+    }
+}
+
+class Obje
+{
+    virtual public void Bilgi()
+    {
+        Console.WriteLine("ben bir objeyim");
+    }
+}
+
+class Terlik : Obje
+{
+    public override void Bilgi()
+    {
+        Console.WriteLine("ben bir terliğim...");
+    }
+}
+
+class Kalem : Obje
+{
+    public override void Bilgi()
+    {
+        Console.WriteLine("ben bir kalemim...");
+    }
+}
