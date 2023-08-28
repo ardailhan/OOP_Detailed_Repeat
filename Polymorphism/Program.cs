83257937256bdee865ae7ed41e7a96faa01b013b
ardailhan <ardailhan_@outlook.com>
2023-08-28 13:08:14 +0300

    Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        C c = new C();
+        var val = c.X;
+        var val2 = c.Y;
+        var val3 = c.Z;
+
+        B b = new C();
+        var value = b.X;
+        var value2 = b.Y;
+
+        A a = new C();
+        var v = a.X;
+    }
+}
+class A
+{
+    public void X() { }
+}
+class B : A
+{
+    public void Y() { }
+}
+class C : B
+{
+    public void Z() { }
+}
