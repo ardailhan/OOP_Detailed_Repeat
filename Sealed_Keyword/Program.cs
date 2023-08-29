153a8aaad30bcf689acc70af578ae9f678b78ef9
ardailhan <ardailhan_@outlook.com>
2023-08-29 13:02:11 +0300

    sealed Keyword

---
+++ Sealed_Keyword/Program.cs
@@ -0,0 +1,17 @@
+﻿C c = new();
+c.X();
+class A
+{
+    public virtual void X() { Console.WriteLine("Merhaba A"); }
+}
+class B : A
+{
+    sealed public override void X()
+    {
+        Console.WriteLine("Merhaba B");
+    }
+}
+class C : B
+{
+    
+}
