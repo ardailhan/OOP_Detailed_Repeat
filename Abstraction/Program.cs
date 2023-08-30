1868f540034bf427ea89f4e8034e01fb61e6a39a
ardailhan <ardailhan_@outlook.com>
2023-08-30 18:49:49 +0300

    Abstraction

---
+++ Abstraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+UserService userService = new();
+IUserValidateService userValidateService = userService;
+
+interface IUserValidateService
+{
+    bool ValidateUser(UserInfo userInfo);
+}
+
+class UserService : IUserValidateService
+{
+    public void CreateUser(UserInfo userInfo)
+    {
+        //..
+    }
+    public void RemoveUser(int userId)
+    {
+        //..
+    }
+    public List<User> Users { get; set; }
+    public bool ValidateUser(UserInfo userInfo)
+    {
+        return true;
+    }
+}
+class UserInfo
+{
+
+}
+class User
+{
+
+}
