7ec54be36a3e7479e4046eab971b9e2561fc90ea
ardailhan <ardailhan_@outlook.com>
2023-09-11 13:33:36 +0300

    Explicit Implementation and Default Implementation

---
+++ InterfaceExample2/Program.cs
@@ -0,0 +1,55 @@
+﻿//Explicity Implement & Name Hiding
+
+MyClass m = new();
+IA a = m;
+a.X();
+
+IB b = m;
+b.X();
+
+class MyClass : IA, IB
+{
+    int IA.X()
+    {
+        Console.WriteLine("A - X");
+        return 0;
+    }
+
+    int IB.X()
+    {
+        Console.WriteLine("B - X");
+        return 1;
+    }
+}
+interface IA
+{
+    int X();
+}
+interface IB
+{
+    int X();
+}
+
+//Default Implementation
+
+class XClass : IZX
+{
+    public void X()
+    {
+        Console.WriteLine("X");
+    }
+    public void Y()
+    {
+        Console.WriteLine("Y"); //Default implementation ezmiş oluyoruz.
+    }
+
+}
+
+interface IZX
+{
+    void X();
+    void Y()
+    {
+        Console.WriteLine("Default Imp. Y"); //Default Implementation
+    }
+}
