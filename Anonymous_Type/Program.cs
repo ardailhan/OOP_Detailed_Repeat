6f10f2a17b9fdff0e6d139fdabe4c32918a42590
ardailhan <ardailhan_@outlook.com>
2023-09-12 14:58:59 +0300

    Anonymous Type

---
+++ Anonymous_Type/Program.cs
@@ -0,0 +1,14 @@
+﻿var person = new
+{
+    Name = "Arda",
+    Surname = "İlhan"
+};
+WritePerson(person);
+
+void WritePerson(dynamic person)
+{
+    Console.WriteLine(person.Name);
+    Console.WriteLine(person.Surname);
+}
+
+Console.WriteLine(person.GetType().FullName);
